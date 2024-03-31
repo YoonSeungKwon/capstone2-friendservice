31650a2fa814907ffaababebb3c2663e9d245a8f
YoonSeungKwon <113295771+YoonSeungKwon@users.noreply.github.com>
2024-03-31 15:46:39 +0900

    프로젝트 생성

---
+++ src/test/java/yoon/docker/friendService/FriendServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package yoon.docker.friendService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FriendServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
